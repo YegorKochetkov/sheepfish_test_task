b42583e403644330614e8519fa9d8b930e36a971
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-03-26 23:10:02 +0300

    feat: persist store to localstorage

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,19 @@ import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider as ReduxProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import './index.css';
-import { store } from './store/store';
+import { persistor, store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<ReduxProvider store={store}>
-			<ChakraProvider>
-				<App />
-			</ChakraProvider>{' '}
+			<PersistGate loading={null} persistor={persistor}>
+				<ChakraProvider>
+					<App />
+				</ChakraProvider>
+			</PersistGate>
 		</ReduxProvider>
 	</React.StrictMode>
 );
