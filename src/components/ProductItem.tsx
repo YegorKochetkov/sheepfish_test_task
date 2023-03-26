0abb408160a8f1d70678ccd8271122c23534fffb
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-03-26 23:59:50 +0300

    feat: add delete functionality to product item

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -2,12 +2,26 @@ import { Button, Center, Image, Td, Tr, useColorMode } from '@chakra-ui/react';
 import React from 'react';
 import { useAppDispatch } from '../store/hooks';
 import { deleteProduct } from '../store/productsSlice';
+import { useDeletePostMutation } from '../store/services/products';
 import ProductItemTextField from './ProductItemTextField';
 import type { ProductType } from '../types/product';
 
 function ProductItem(product: ProductType) {
+	if (product.isDeleted) {
+		return null;
+	}
+
 	const { colorMode } = useColorMode();
 	const dispatch = useAppDispatch();
+	const [deletePost, { isLoading }] = useDeletePostMutation();
+
+	const handleDeleteProducts = async (id: number) => {
+		const response = await deletePost(id);
+
+		if ('data' in response) {
+			dispatch(deleteProduct(response.data));
+		}
+	};
 
 	return (
 		<Tr
@@ -31,7 +45,7 @@ function ProductItem(product: ProductType) {
 			<Td textAlign='center'>{product.price}</Td>
 			<Td
 				_hover={{
-					':hover img': {
+					img: {
 						width: '10rem',
 						height: 'auto',
 						'z-index': '10',
@@ -61,7 +75,14 @@ function ProductItem(product: ProductType) {
 				</Button>
 			</Td>
 			<Td>
-				<Button colorScheme='red' size='sm'>
+				<Button
+					opacity={isLoading ? '0.2' : '1'}
+					cursor={isLoading ? 'inherit' : 'pointer'}
+					colorScheme='red'
+					size='sm'
+					onClick={() => handleDeleteProducts(product.id)}
+					disabled={isLoading}
+				>
 					Delete
 				</Button>
 			</Td>
