06522d3721a34403c2b3bc6dd2adfded0b0e6081
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-03-28 10:42:01 +0300

    refactor:  handleSort use url params now
    we don't get handleSort from props anymore

--- src/components/ProductsListControl.tsx
+++ src/components/ProductsListControl.tsx
@@ -1,7 +1,8 @@
-import { HTMLChakraProps, Th, useColorMode } from '@chakra-ui/react';
-import React, { useContext } from 'react';
-import { ProductType } from '../types/product';
-import { DataSortContext } from './ProductsList';
+import { HTMLChakraProps, Th, useColorMode } from "@chakra-ui/react";
+import React, { useContext } from "react";
+import { useSearchParams } from "react-router-dom";
+import { ProductType } from "../types/product";
+import { DataSortContext } from "./ProductsList";
 
 type ProductsListControlProps = HTMLChakraProps<'th'> & {
 	field: keyof ProductType;
@@ -13,12 +14,28 @@ function ProductsListControl({
 }: ProductsListControlProps) {
 	const { colorMode } = useColorMode();
 	const dataSort = useContext(DataSortContext);
+	const [_searchParams, setSearchParams] = useSearchParams();
+
+	let direction: 'asc' | 'desc' = 'asc';
+
+	if (dataSort?.currSortBy === field) {
+		direction = dataSort?.orderBy === 'asc' ? 'desc' : 'asc';
+	}
+
+	const handleSort = () => {
+		setSearchParams((search) => {
+			search.set('sortby', field);
+			search.set('orderby', direction);
+
+			return search;
+		});
+	};
 
 	return (
 		<Th
 			{...otherProps}
 			transition='background 0.3s'
-			onClick={() => dataSort?.handleSort(field)}
+			onClick={handleSort}
 			_hover={{
 				bg: `${colorMode === 'light' ? 'gray.200' : 'gray.600'}`,
 				cursor: 'pointer',
@@ -26,7 +43,7 @@ function ProductsListControl({
 		>
 			{field}{' '}
 			{dataSort?.currSortBy === field &&
-				(dataSort?.sortOrder === 'asc' ? <>&uarr;</> : <>&darr;</>)}
+				(dataSort?.orderBy === 'asc' ? <>&uarr;</> : <>&darr;</>)}
 		</Th>
 	);
 }
