7066b29323e780d93d081ba75892204415aae635
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-03-30 01:06:00 +0300

    feat: add modal to header
    update header css styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,50 @@
+import { AddIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import React from "react";
+import { useParams } from "react-router-dom";
+import ProductForm from "./Form/ProductForm";
+import Modal from "./Modal";
+import Search from "./Search";
 import {
 	Button,
 	Grid,
 	Heading,
-	useColorMode
-	} from "@chakra-ui/react";
-import React from "react";
-import Search from "./Search";
+	useColorMode,
+	useDisclosure,
+} from '@chakra-ui/react';
 
 function Header() {
 	const { toggleColorMode, colorMode } = useColorMode();
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const { productId } = useParams();
 
 	return (
-		<Grid templateColumns='1fr 1fr min-content' gap={6} marginTop={6}>
-			<Heading minWidth='fit-content'>Products list</Heading>
-			<Search />
-			<Button onClick={toggleColorMode} minWidth='min-content'>
-				{colorMode === 'light' ? 'Dark' : 'Light'} Mode
+		<Grid
+			templateColumns='1fr 1fr min-content min-content'
+			gap={6}
+			marginTop={6}
+		>
+			<Heading minWidth='fit-content'>{productId ?? 'Products list'}</Heading>
+			{!productId && <Search />}
+
+			<Button
+				onClick={onOpen}
+				title='add new product'
+				maxWidth='min-content'
+				marginLeft='auto'
+			>
+				<AddIcon />
+			</Button>
+
+			<Modal onClose={onClose} isOpen={isOpen} title='add new product'>
+				<ProductForm onClose={onClose} />
+			</Modal>
+
+			<Button
+				onClick={toggleColorMode}
+				maxWidth='min-content'
+				title='toggle theme color'
+			>
+				{colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
 			</Button>
 		</Grid>
 	);
