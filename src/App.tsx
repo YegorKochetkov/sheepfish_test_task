5431f1fa608ae43a805f9f73b2dc66cde180d23b
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-03-30 01:19:40 +0300

    chore: remove redundant css styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import Header from "./components/Header";
 
 function App() {
 	return (
-		<Box height='100dvh'>
-			<Container maxWidth='min' minWidth='75rem'>
-				<Header />
-				<Box as='main' mt={6}>
-					<Outlet />
-				</Box>
-			</Container>
-		</Box>
+		<Container maxWidth='min' minWidth='75rem'>
+			<Header />
+			<Box as='main' mt={6}>
+				<Outlet />
+			</Box>
+		</Container>
 	);
 }
 
